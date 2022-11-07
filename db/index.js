79c1e4fd488b0c89f8015bdd9052598a9bf8fb05
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-11-06 20:40:32 -0600

    index.js added

--- db/index.js
+++ db/index.js
@@ -1,1 +1,35 @@
-const { client } = require("./index");
+// Step 1: Import pg from the dependency from index.js
+const { Client } = require('pg');
+
+// Step 2: Create a new pg.Client
+const client = new Client('postgres://localhost:5432/juicebox-dev'); 
+
+// User Methods
+
+// Method #1: Test
+async function testDB() {
+    try {
+      // Connect the client to the database
+      client.connect();
+  
+      // Queries are promises, so we can await them
+      const result = await client.query(`SELECT * FROM users;`);
+  
+      // Logging is a fine way to see what's up
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      // Close out the client connection
+      client.end();
+    }
+  }
+  
+  testDB();
+
+
+
+// Step Final: Export our client variable 
+module.exports = {
+    client
+}
--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,3 @@
-// Step 1: Import pg from the dependency from index.js
-const { client } = require("./index");
+const {rebuildDB} = require("./index.js");
 
-// Step 2: Create a new pg.Client
-const client = new pg.Client('postgres://localhost:5432/juicebox-dev'); 
-
-// STEP 3: Export our client variable 
-module.exports = {
-    client
-}
+// rebuildDB()
