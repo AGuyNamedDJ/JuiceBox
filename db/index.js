2e5136bd5afff1a1ff863fff6842194aff809356
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-11-06 20:51:16 -0600

    getUSersFN added to index and seed

--- db/index.js
+++ db/index.js
@@ -4,32 +4,20 @@ const { Client } = require('pg');
 // Step 2: Create a new pg.Client
 const client = new Client('postgres://localhost:5432/juicebox-dev'); 
 
-// User Methods
-
-// Method #1: Test
-async function testDB() {
-    try {
-      // Connect the client to the database
-      client.connect();
+// Step 3: Helper Functions
+    // getAllUsers
+async function getAllUsers() {
+    const { rows } = await client.query(
+      `SELECT id, username 
+      FROM users;
+    `);
   
-      // Queries are promises, so we can await them
-      const result = await client.query(`SELECT * FROM users;`);
-  
-      // Logging is a fine way to see what's up
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      // Close out the client connection
-      client.end();
-    }
+    return rows;
   }
   
-  testDB();
 
-
-
-// Step Final: Export our client variable 
+// Step Final: Export our client variable & other FNs
 module.exports = {
-    client
+    client,
+    getAllUsers,
 }
--- db/seed.js
+++ db/seed.js
@@ -1,3 +1,29 @@
-const {rebuildDB} = require("./index.js");
+// Step 1: Import client from the export index.js
+    // You can add all the import FNs here
+const { client,
+getAllUsers,
+ } = require('./index');
 
-// rebuildDB()
+// Step 2: User Methods
+// Method #1: Test
+async function testDB() {
+    try {
+      // Step 2a: Connect the client to the database
+      client.connect();
+  
+      // Step 2b: Queries are promises, so we can await them
+      const users = await getAllUsers();
+  
+      // Step 2c: Logging is a fine way to see what's up
+      console.log(users);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      // Step 2d: Close out the client connection
+      client.end();
+    }
+  }
+  
+  testDB();
+
+
