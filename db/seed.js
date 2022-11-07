5d7779b0700af37ab4975c6f547d5e5a0eff3483
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-11-06 21:14:09 -0600

    part 1 almost done

--- db/seed.js
+++ db/seed.js
@@ -13,69 +13,49 @@
 
 // Step 1: Import client from the export index.js
     // You can add all the import FNs here
-const { client,
-getAllUsers,
- } = require('./index');
-
+const {
+    client,
+    getAllUsers
+  } = require('./index');
 
 // Step 2: User Methods
-
- // Method: createTables
-async function createTables() {
+  
+    // Method: dropTables
+  async function dropTables() {
     try {
-    console.log("Starting to build tables...");
-
+      console.log("Starting to drop tables...");
+  
       await client.query(`
-      CREATE TABLE users (
-        id SERIAL PRIMARY KEY,
-        username varchar(255) UNIQUE NOT NULL,
-        password varchar(255) NOT NULL
-      );
+        DROP TABLE IF EXISTS users;
       `);
-
+  
+      console.log("Finished dropping tables!");
+    } catch (error) {
+      console.error("Error dropping tables!");
+      throw error;
+    }
+  }
+  
+    // Method: createTables
+  async function createTables() {
+    try {
+      console.log("Starting to build tables...");
+  
+      await client.query(`
+        CREATE TABLE users (
+          id SERIAL PRIMARY KEY,
+          username varchar(255) UNIQUE NOT NULL,
+          password varchar(255) NOT NULL
+        );
+      `);
+  
       console.log("Finished building tables!");
     } catch (error) {
-        console.error("Error building tables!");
-        throw error; // we pass the error up to the function that calls createTables
+      console.error("Error building tables!");
+      throw error;
     }
   }
-
-    // Method: dropTables
-async function dropTables() {
-  try {
-    console.log("Starting to drop tables...");
-
-    await client.query(`
-    DROP TABLE IF EXISTS users;
-    `);
-
-    console.log("Finished dropping tables!");
-  } catch (error) {
-    console.error("Error dropping tables!");
-    throw error; // we pass the error up to the function that calls dropTables
-  }
-}
-
-    // Method: testTables
-async function testDB() {
-    try {
-      // Step 2a: Connect the client to the database
-      client.connect();
   
-      // Step 2b: Queries are promises, so we can await them
-      const users = await getAllUsers();
-  
-      // Step 2c: Logging is a fine way to see what's up
-      console.log(users);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      // Step 2d: Close out the client connection
-      client.end();
-    }
-  }
-  testDB();
-
   // Method: rebuildDB
   async function rebuildDB() {
     try {
@@ -87,8 +67,8 @@ async function testDB() {
       throw error;
     }
   }
-
-    // ethod: testDB
+  
+    // Method: testTables
   async function testDB() {
     try {
       console.log("Starting to test database...");
@@ -103,7 +83,8 @@ async function testDB() {
     }
   }
   
+  
   rebuildDB()
-  .then(testDB)
-  .catch(console.error)
-  .finally(() => client.end());
+    .then(testDB)
+    .catch(console.error)
+    .finally(() => client.end());
