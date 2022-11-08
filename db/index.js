80e2fafd1d16e6cee52306233f49282a935aaf2f
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-11-08 15:49:44 -0600

    updates

--- db/index.js
+++ db/index.js
@@ -13,11 +13,14 @@ const client = new Client('postgres://localhost:5432/juicebox-dev');
     // FN: createUser
 async function createUser({ username, password }) {
   try {
-    const result = await client.query(`
+    const { rows } = await client.query(`
+      INSERT INTO users(username, password) 
+      VALUES($1, $2) 
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
+    `, [username, password]);
 
-    `);
-
-    return result
+    return rows;
   } catch (error) {
     throw error;
   }
