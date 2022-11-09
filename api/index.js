993afcf86132d740fcce6ddb4a9806cc3377313d
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-11-09 13:02:39 -0600

    lucnh stopping point

--- api/index.js
+++ api/index.js
@@ -4,19 +4,59 @@ const express = require('express');
 const apiRouter = express.Router();
 const usersRouter = require('./users');
 
-// Port # 
-const PORT = 3000;
+const jwt = require('jsonwebtoken');
+const { getUserById } = require('../db');
+const { JWT_SECRET } = process.env;
 
-const express = require('express');
-const server = express();
+// set `req.user` if possible; MIddleware read JWT
+apiRouter.use(async (req, res, next) => {
+    const prefix = 'Bearer ';
+    const auth = req.header('Authorization');
+  
+    if (!auth) { // nothing to see here
+        // IF: The Authorization header wasn't set. This might happen with registration or login, or when the browser doesn't have a saved token. Regardless of why, there is no way we can set a user if their data isn't passed to us.
+      next();
+    } else if (auth.startsWith(prefix)) {
+        // ELSE IF: It was set, and begins with Bearer followed by a space. If so, we'll read the token and try to decrypt it. a. On successful verify, try to read the user from the database b. A failed verify throws an error, which we catch in the catch block. We read the name and message on the error and pass it to next()
+      const token = auth.slice(prefix.length);
+  
+      try {
+        const { id } = jwt.verify(token, JWT_SECRET);
+  
+        if (id) {
+          req.user = await getUserById(id);
+          next();
+        }
+      } catch ({ name, message }) {
+        next({ name, message });
+      }
+    } else {
+        // ELSE: A user set the header, but it wasn't formed correctly. We send a name and message to next()
+      next({
+        name: 'AuthorizationHeaderError',
+        message: `Authorization token must start with ${ prefix }`
+      });
+    }
+  });
 
-// Listener
-server.listen(PORT, () => {
-  console.log('The server is up on port', PORT)
+  // Middleware
+apiRouter.use((req, res, next) => {
+  if (req.user) {
+    console.log("User is set:", req.user);
+  }
+
+  next();
 });
+  
+  // Routers are below
 
-apiRouter.use('/users', usersRouter);
-
+  // Router above
+  apiRouter.use((error, req, res, next) => {
+    res.send({
+      name: error.name,
+      message: error.message
+    });
+  });
 
 // Export
 module.exports = apiRouter;
--- api/users.js
+++ api/users.js
@@ -21,5 +21,37 @@ usersRouter.get('/', async (req, res) => {
       users
     });
   });
+
+  // Login
+usersRouter.post('/login', async (req, res, next) => {
+  const { username, password } = req.body;
+
+  // request must have both
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password"
+    });
+  }
+
+  try {
+    const user = await getUserByUsername(username);
+
+    if (user && user.password == password) {
+      // create token & return to user
+      res.send({ message: "you're logged in!" });
+    } else {
+      next({ 
+        name: 'IncorrectCredentialsError', 
+        message: 'Username or password is incorrect'
+      });
+    }
+  } catch(error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+
 // Export
 module.exports = usersRouter;
