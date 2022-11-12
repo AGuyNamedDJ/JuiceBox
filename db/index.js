4e00d30a159fc5bb6ee57526321b146e16440721
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-11-12 17:59:46 -0600

    updates for deploying

--- db/index.js
+++ db/index.js
@@ -126,6 +126,7 @@ async function createPost({
   }
 }
 
+// updatePost
 async function updatePost(postId, fields = {}) {
   // read off the tags & remove that field 
   const { tags } = fields; // might be undefined
@@ -201,6 +202,13 @@ async function getPostById(postId) {
       FROM posts
       WHERE id=$1;
     `, [postId]);
+
+    if (!post) {
+      throw {
+        name: "PostNotFoundError",
+        message: "Could not find a post with that postId"
+      };
+    }
 
     const { rows: tags } = await client.query(`
       SELECT tags.*
--- index.js
+++ index.js
@@ -44,3 +44,18 @@ server.use((req, res, next) => {
   });
 
   server.use("/api", apiRouter);
+
+// Background Color Route
+  server.get('/background/:color', (req, res, next) => {
+    res.send(`
+      <body style="background: ${ req.params.color };">
+        <h1>Hello World</h1>
+      </body>
+    `);
+  });
+
+  server.get('/add/:first/to/:second', (req, res, next) => {
+    res.send(`<h1>${ req.params.first } + ${ req.params.second } = ${
+      Number(req.params.first) + Number(req.params.second)
+     }</h1>`);
+  });
